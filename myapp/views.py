37fa7c24428816df637e94401bb46617c03bcf81
karthik <karthikramasamy90@gmail.com>
2026-02-24 12:54:24 +0530

    auth

--- myapp/views.py
+++ myapp/views.py
@@ -2,8 +2,46 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.contrib.auth import authenticate
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from .models import Product
 # Create your views here.
+
+@api_view(['POST'])
+def login_view(request):
+    username = request.data.get("username")
+    password = request.data.get("password")
+
+    user = authenticate(username=username, password=password)
+
+    if user is not None:
+        # Create token if not exists
+        token, created = Token.objects.get_or_create(user=user)
+
+        return Response({
+            "token": token.key,
+            "user_id": user.id,
+            "username": user.username
+        })
+
+    return Response(
+        {"error": "Invalid credentials"},
+        status=status.HTTP_401_UNAUTHORIZED
+    )
+    
+@api_view(['GET'])
+def dashboard(request):
+    return Response({
+        "message": "Welcome",
+        "user": request.user.username
+    })
+
 class ProductListView(APIView):
 
     def get(self, request):
