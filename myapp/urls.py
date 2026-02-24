a94110ce4dafa7023402a06e71c8ab6686c203c7
karthik <karthikramasamy90@gmail.com>
2026-02-24 13:07:11 +0530

    urls

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ProductListView
+from .views import login_view, ProductListView
 
 urlpatterns = [
-    path('products/', ProductListView.as_view()),
+    path('login/', login_view, name='login'),
+    path('products/', ProductListView.as_view(), name='products'),
 ]
