e9da2126947606de059623c0623d4ae5f0401969
karthik <karthikramasamy90@gmail.com>
2026-02-24 12:42:27 +0530

    modelchanges

--- myapp/models.py
+++ myapp/models.py
@@ -4,7 +4,16 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=500, null=True, blank=True)
+    description = models.CharField(max_length=500, null=True, blank=True)
     price = models.CharField(max_length=500, null=True, blank=True)
+    brand = models.CharField(max_length=500, null=True, blank=True)
+    category = models.CharField(max_length=500, null=True, blank=True)
+    specifications = models.JSONField()
+    rating = models.CharField(max_length=500, null=True, blank=True)
+    review_count = models.CharField(max_length=500, null=True, blank=True)
+    amazon_url = models.CharField(max_length=500, null=True, blank=True)
+    availability = models.CharField(max_length=500, null=True, blank=True)
+    market_analysis = models.CharField(max_length=500, null=True, blank=True)
 
     def __str__(self):
         return self.name
